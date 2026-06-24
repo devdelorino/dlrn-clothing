76e79b6fbfb8422567e7bcd93e754d8646610991
devdelorino <delorinodwnvwr@gmail.com>
2026-06-24 23:33:38 +0800

    reset

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,9 +1,16 @@
 import { products } from '../data/products.js';
 
-let productHTML = '';
+displayProducts();
 
-products.forEach((product) => {
-  productHTML += `
+function displayProducts() {
+  let tShirtHTML = '';
+  let shortsHTML = '';
+  let hoodieHTML = '';
+  let jeansHTML = '';
+
+  products.forEach((product) => {
+    if (product.category === 't-shirts') {
+      tShirtHTML += `
     <div class="product-container">
       <img src="assets/images/products/${product.image}"
         class="product-image">
@@ -15,6 +22,50 @@ products.forEach((product) => {
       </div>
     </div>
   `;
-});
+    } else if (product.category === 'shorts') {
+      shortsHTML += `
+    <div class="product-container">
+      <img src="assets/images/products/${product.image}"
+        class="product-image">
+      <div class="product-name">
+        ${product.name}
+      </div>
+      <div class="product-price">
+        ₱${product.price}
+      </div>
+    </div>
+  `;
+    } else if (product.category === 'hoodie') {
+      hoodieHTML += `
+    <div class="product-container">
+      <img src="assets/images/products/${product.image}"
+        class="product-image">
+      <div class="product-name">
+        ${product.name}
+      </div>
+      <div class="product-price">
+        ₱${product.price}
+      </div>
+    </div>
+  `;
+    } else if (product.category === 'jeans') {
+      jeansHTML += `
+    <div class="product-container">
+      <img src="assets/images/products/${product.image}"
+        class="product-image">
+      <div class="product-name">
+        ${product.name}
+      </div>
+      <div class="product-price">
+        ₱${product.price}
+      </div>
+    </div>
+  `;
+    }
+  });
 
-document.querySelector('.products-grid').innerHTML = productHTML;
+  document.querySelector('.js-t-shirts-grid').innerHTML = tShirtHTML;
+  document.querySelector('.js-shorts-grid').innerHTML = shortsHTML;
+  document.querySelector('.js-hoodie-grid').innerHTML = hoodieHTML;
+  document.querySelector('.js-jeans-grid').innerHTML = jeansHTML;
+}
