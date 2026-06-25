198a619e20e42a53a003b8a026a2838389484af0
devdelorino <delorinodwnvwr@gmail.com>
2026-06-25 22:33:32 +0800

    modified products

--- assets/data/products.js
+++ assets/data/products.js
@@ -1,100 +1,98 @@
 export const products = [
   {
-    id: 'prod-mk8z1a-001',
-    category: 'hoodie',
-    image: 'acdc-classic-logo-with-lightning-bolt-mens-premium-t-shirt.jpg',
-    name: 'AC/DC Classic Logo With Lightning Bolt',
-    price: 799
+    id: ObjectId('6a3d3915dce0e3e2a5abc114'),
+    category: 'tees',
+    name: 'Essential Pocket Tee',
+    price: 30
   },
   {
-    id: 'prod-x9k2p3-002',
-    category: 'shorts',
-    image: 'adventure-time-vintage-jake-unisex-crewneck-sweatshirt.jpg',
-    name: 'Adventure Time Vintage Jake',
-    price: 1190
+    id: ObjectId('6a3d3915dce0e3e2a5abc115'),
+    category: 'tees',
+    name: 'Heavyweight Box Tee',
+    price: 35
   },
   {
-    id: 'prod-7qz9xa-003',
-    category: 'jeans',
-    image: 'america-est-1776-womens-cropped-t-shirt.jpg',
-    name: 'America Est 1776',
-    price: 650
+    id: ObjectId('6a3d3915dce0e3e2a5abc116'),
+    category: 'tees',
+    name: 'Vintage Wash Graphic Tee',
+    price: 40
   },
   {
-    id: 'prod-a81m2v-004',
-    category: 'hoodie',
-    image: 'america-land-of-the-free-mens-premium-t-shirt.jpg',
-    name: 'America Land Of The Free',
-    price: 700
+    id: ObjectId('6a3d3915dce0e3e2a5abc117'),
+    category: 'tees',
+    name: 'Long Sleeve Thermal',
+    price: 45
   },
   {
-    id: 'prod-kp9x7z-005',
-    category: 'hoodie',
-    image: 'america-the-beautiful-est-1776-unisex-poly-cotton-t-shirt.jpg',
-    name: 'America The Beautiful Est 1776',
-    price: 750
+    id: ObjectId('6a3d3915dce0e3e2a5abc118'),
+    category: 'bottoms',
+    name: 'Utility Short',
+    price: 55
   },
   {
-    id: 'prod-zm2c8v-006',
-    category: 't-shirts',
-    image: 'asterix-obelix-asterix-fist-washed-womens-muscle-tank-top.jpg',
-    name: 'Asterix & Asterix Fist',
-    price: 690
+    id: ObjectId('6a3d3915dce0e3e2a5abc119'),
+    category: 'bottoms',
+    name: 'Standart Sweatpant',
+    price: 65
   },
   {
-    id: 'prod-t7n4q1-007',
-    category: 't-shirts',
-    image: 'us-flag-mens-organic-t-shirt.jpg',
-    name: 'US Flag',
-    price: 640
+    id: ObjectId('6a3d3915dce0e3e2a5abc11a'),
+    category: 'bottoms',
+    name: 'Relaxed Cargo Pand',
+    price: 90
   },
   {
-    id: 'prod-b9x3md-008',
-    category: 'jeans',
-    image: 'batman-with-bats-and-red-batman-logo-unisex-oversized-heavyweight-t-shirt.jpg',
-    name: 'Batman With Bats And Red Batman Logo',
-    price: 590
+    id: ObjectId('6a3d3915dce0e3e2a5abc11b'),
+    category: 'bottoms',
+    name: 'Raw Denim Jean',
+    price: 120
   },
   {
-    id: 'prod-h2k8zp-009',
-    category: 't-shirts',
-    image: 'classic-1965-57th-birthday-retro-vintage-mens-premium-t-shirt.jpg',
-    name: 'Classic 1965 - 57th birthday Retro Vintage',
-    price: 620
+    id: ObjectId('6a3d3915dce0e3e2a5abc11c'),
+    category: 'outerwear',
+    name: 'Quarter-Zip Fleece',
+    price: 75
   },
   {
-    id: 'prod-qw7x1a-010',
-    category: 'jeans',
-    image: 'dc-comics-batman-logo-used-look-mens-hoodie.jpg',
-    name: 'DC Comics Batman Logo Used Look',
-    price: 999
+    id: ObjectId('6a3d3915dce0e3e2a5abc11d'),
+    category: 'outerwear',
+    name: 'Structure Hoodie',
+    price: 85
   },
   {
-    id: 'prod-lm3v8c-011',
-    category: 'shorts',
-    image: 'game-of-thrones-swords-of-the-kingdoms-mens-t-shirt.jpg',
-    name: 'Game Of Thrones Swords Of The Kingdoms',
-    price: 890
+    id: ObjectId('6a3d3915dce0e3e2a5abc11e'),
+    category: 'outerwear',
+    name: 'Nylon Puffer Vest',
+    price: 95
   },
   {
-    id: 'prod-xc9p2n-012',
-    category: 'shorts',
-    image: 'harry-potter-hogwarts-coat-of-arms-mens-hoodie.jpg',
-    name: 'Harry Potter Hogwarts Coat of Arms',
-    price: 1120
+    id: ObjectId('6a3d3915dce0e3e2a5abc11f'),
+    category: 'outerwear',
+    name: 'Technical Overshirt',
+    price: 110
   },
   {
-    id: 'prod-vk8z7q-013',
-    category: 'hoodie',
-    image: 'outdoor-boys-logo-mens-ringer-t-shirt.jpg',
-    name: 'Outdoor Boys Logo',
-    price: 520
+    id: ObjectId('6a3d3915dce0e3e2a5abc120'),
+    category: 'headwear',
+    name: 'Dad Hat (Unstructured)',
+    price: 30
   },
   {
-    id: 'prod-r2m9xa-014',
-    category: 'hoodie',
-    image: 'the-hobbit-characters-around-bilbo-baggins-mens-organic-t-shirt.jpg',
-    name: 'The Hobbit Characters Around Bilbo Baggins',
-    price: 650
+    id: ObjectId('6a3d3915dce0e3e2a5abc121'),
+    category: 'headwear',
+    name: '5-Panel Performance Cap',
+    price: 35
+  },
+  {
+    id: ObjectId('6a3d3915dce0e3e2a5abc122'),
+    category: 'headwear',
+    name: 'Merino Beanie',
+    price: 40
+  },
+  {
+    id: ObjectId('6a3d3915dce0e3e2a5abc123'),
+    category: 'headwear',
+    name: 'Ripstop Bucket Hat',
+    price: 45
   }
 ];
