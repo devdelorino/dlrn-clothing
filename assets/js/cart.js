048ca771a1c7154dd64039879a47bdb31bcc2f0d
devdelorino <delorinodwnvwr@gmail.com>
2026-06-30 16:47:44 +0800

    added order summary interactiveness

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,90 +1,133 @@
-// import { cart } from "../data/cart.js";
-// import { products } from "../data/products.js";
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
-// renderCart();
+renderCart();
 
-// function renderCart() {
-//   if (cart.length === 0) {
+function renderCart() {
+  if (cart.length === 0) {
 
-//     document.querySelector('.js-cart-container').innerHTML = `
-//     <div class=empty-cart-container>
-//       <h1 class="empty-cart-title">
-//         CART
-//       </h1>
-//       <p class="empty-cart-description-text">
-//         Your cart is currently empty.
-//       </p>
-//       <a class="empty-shop-link" href="shop.html">Continue shopping</a>
-//     </div>
-//   `;
+    document.querySelector('.js-cart-container').innerHTML = `
+    <div class=empty-cart-container>
+      <h1 class="empty-cart-title">
+        CART
+      </h1>
+      <p class="empty-cart-description-text">
+        Your cart is currently empty.
+      </p>
+      <a class="empty-shop-link" href="shop.html">Continue shopping</a>
+    </div>
+  `;
 
-//   } else {
-//     let cartHTML = `
-//     <h3 class="shopping-bag-title">
-//       YOUR CART
-//     </h3>
-//   `;
+  } else {
+    let subtotal = 0;
+    let shipping = 10;
+    let total = 0;
+    let cartHTML = `
+    <h3 class="shopping-bag-title">
+      YOUR CART
+    </h3>
+  `;
 
-//     cart.forEach((cart) => {
-//       products.forEach((product) => {
-//         if (cart.id === product.id) {
-//           cartHTML += `
-//           <div class="product-container">
-//             <div class="row-1-product-container">
-//               <div class="product-image-container">
-//                 <img src="assets/images/products/${product.image}" class="product-image">
-//               </div>
-//               <h3 class="product-name">
-//                 ${product.name}
-//               </h3>
-//               <h2>
-//                 $${product.price}
-//               </h2>
-//             </div>
-//             <div class="row-2-product-container">
-//               <div class="js-delete-button" data-id="${cart.id}">
-//                 <img src="assets/images/icons/delete-icon.svg">
-//               </div>
-//               <h4 class="size-text">
-//                 ${cart.size}
-//               </h4>
-//               <div class="button-container">
-//                 <div class="add-button">
-//                   +
-//                 </div>
-//                 <input type="number" class="quantity-input" value="${cart.quantity}">
-//                 <div class="subtract-button">
-//                   -
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         `;
-//         }
-//       });
-//     });
+    cart.forEach((cart) => {
+      products.forEach((product) => {
+        if (cart.id === product.id) {
+          cartHTML += `
+          <div class="product-container">
+            <div class="row-1-product-container">
+              <div class="product-image-container">
+                <img src="assets/images/products/${product.image}" class="product-image">
+              </div>
+              <h3 class="product-name">
+                ${product.name}
+              </h3>
+              <h2>
+                $${product.price}
+              </h2>
+            </div>
+            <div class="row-2-product-container">
+              <div class="js-delete-button" data-id="${cart.id}">
+                <img src="assets/images/icons/delete-icon.svg">
+              </div>
+              <h4 class="size-text">
+                ${cart.size}
+              </h4>
+              <div class="button-container">
+                <div class="add-button">
+                  +
+                </div>
+                <input type="number" class="quantity-input" value="${cart.quantity}">
+                <div class="subtract-button">
+                  -
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
 
-//     document.querySelector('.js-cart-container').innerHTML = cartHTML;
+          subtotal += product.price * cart.quantity;
+        }
+      });
+    });
 
-//     deleteCart();
-//   }
-// }
+    total = subtotal + shipping;
+
+    cartHTML += `
+      <div class="order-summary-container">
+        <h2 class="order-summary-title">
+          ORDER SUMMARY
+        </h2>
+        <div class="subtotal-container">
+          <h4 class="subtotal-text">
+            Subtotal
+          </h4>
+          <h3 class="subtotal-price">
+            $${subtotal}
+          </h3>
+        </div>
+        <div class="shipping-container">
+          <h4 class="shipping-text">
+            Shipping
+          </h4>
+          <h3 class="shipping-price">
+            $${shipping}
+          </h3>
+        </div>
+        <div class="total-container">
+          <h3>
+            TOTAL
+          </h3>
+          <h3>
+            $${total}
+          </h3>
+        </div>
+
+        <button class="checkout-button">
+          CHECKOUT
+        </button>
+      </div>
+    `;
+
+    document.querySelector('.js-cart-container').innerHTML = cartHTML;
+
+    deleteCart();
+  }
+}
 
 
 
-// /*----- DELETE PRODUCT FROM CART -----*/
-// function deleteCart() {
-//   document.querySelectorAll('.js-delete-button').forEach((deleteButton, index) => {
-//     deleteButton.addEventListener('click', () => {
-//       const deleteId = deleteButton.dataset.id;
+/*----- DELETE PRODUCT FROM CART -----*/
+function deleteCart() {
+  document.querySelectorAll('.js-delete-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      const deleteId = deleteButton.dataset.id;
 
-//       cart.splice(index, 1);
+      cart.splice(index, 1);
 
-//       console.log(cart);
+      console.log(cart);
 
-//       localStorage.setItem('localStorageCart', JSON.stringify(cart));
+      localStorage.setItem('localStorageCart', JSON.stringify(cart));
 
-//       renderCart();
-//     });
-//   });
-// }
+      renderCart();
+    });
+  });
+}
